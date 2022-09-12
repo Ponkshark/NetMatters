0ebbeca340e0e7ded4af5807aa2fd387a7908eb9
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-09-12 16:31:02 +0100

    - Now cuts off excess images, but still does not work on small screen sizes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
       $('.slider-container').slick({
         slidesToShow: 5,
+        centerMode: true,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 1000,
         arrows: false,
         draggable: true,
         infinite: true,
