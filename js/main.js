6e4cda51aba0c2fa12d61f4c3f330d7d46845168
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-09-12 16:27:31 +0100

    Javascript added
    Added javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+      $('.slider-container').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        draggable: true,
+        infinite: true,
+      });
+});
+    
+
