7e6df32aba5554208bbc316810282d9c7ef5dee4
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-09-13 16:44:28 +0100

    - Have created the javascript to add the sticky nav bar on scroll up - Have changed the padding of the websites body in the media queries - Have added a hamburger menu button, with the javascript to animate it.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+// Carousel
+
 $(document).ready(function(){
       $('.slider-container').slick({
         slidesToShow: 5,
@@ -10,5 +13,27 @@ $(document).ready(function(){
         infinite: true,
       });
 });
-    
+   
+// Sticky Nav Bar on Scroll
+
+window.onscroll = function() {
+  scroll();
+};
+
+var oldScrollY = window.scrollY;
+window.onscroll = function() {
+  if(oldScrollY < window.scrollY){
+    if(window.scrollY > 110){
+    document.getElementById("main-header-id").style.top = "-110px";
+    document.getElementById("main-nav-id").style.top = "-110px";
+    document.getElementById("form-2-main-cont").style.top = "-110px";
+    }
+  } else {
+    document.getElementById("main-header-id").style.top = "0";
+    document.getElementById("main-nav-id").style.top = "120px";
+    document.getElementById("form-2-main-cont").style.top = "80px";
+  }
+  oldScrollY = window.scrollY;
+}
+
 
