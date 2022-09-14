cb69244c9cbe6de0a5ae12fc5378746467708904
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-09-14 10:40:17 +0100

    - Removed slick carousel + files - Added owl carousel js and css - Applied owl carousel to the slider - Added JavaScript to impliment owl carousel - Added JavaScript to have it behave as needed

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@
 // Carousel
 
 $(document).ready(function(){
-      $('.slider-container').slick({
-        slidesToShow: 5,
-        centerMode: true,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        draggable: true,
-        infinite: true,
-      });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 100,
+    autoWidth: true,
+    autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      items: 8
+  });
 });
+
+
    
 // Sticky Nav Bar on Scroll
 
