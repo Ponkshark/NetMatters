47e1a739bdc0dd41ba072bdbc111d760e68481de
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-09-15 10:15:09 +0100

    - Finished the stylings of the sidebar - Included a final button at the bottom on small screen sizes - Changed the way the sidebar loads in front width 0 -> right: -350px so the text does not get squished on sidebar load in and out.

--- js/main.js
+++ js/main.js
@@ -40,14 +40,14 @@ window.onscroll = function() {
 //Collapsible Sidebar
 
 function openNav() {
-  document.getElementById("sidebar-inner-id").style.width = "350px";
+  document.getElementById("sidebar-inner-id").style.right = "0px";
   document.getElementById("body-id").style.marginRight = "350px";
   document.getElementById("body-id").style.marginLeft = "-350px";
   document.getElementById("body-id").style.overflow = "hidden";
 }
 
 function closeNav() {
-  document.getElementById("sidebar-inner-id").style.width = "0";
+  document.getElementById("sidebar-inner-id").style.right = "-350px";
   document.getElementById("body-id").style.marginRight = "0";
   document.getElementById("body-id").style.marginLeft = "0";
   document.getElementById("body-id").style.overflow = "scroll";
